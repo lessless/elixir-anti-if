80f6770140d20804396a4f5801c9c895f71de8b9
Yevhenii Kurtov <yevhenii@kurtov.pro>
2024-10-21 22:12:09 +0100

    Instal excoverall

--- mix.exs
+++ mix.exs
@@ -2,8 +2,25 @@ defmodule GildedRose.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gilded_rose,
-     version: "0.0.1",
-     elixir: "~> 1.0"]
+    [
+      app: :gilded_rose,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
+    ]
+  end
+
+  defp deps do
+    [
+      {:excoveralls, "~> 0.18", only: :test}
+    ]
   end
 end
